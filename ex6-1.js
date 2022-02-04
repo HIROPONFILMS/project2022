68247599671f89fa4a48939694953f11349d6251
HIROPONFILMS <zm19291@josai.ac.jp>
2022-02-04 13:48:02 +0000

    2345

--- ex6-1.js
+++ ex6-1.js
@@ -1,2 +1,2 @@
-function imgchange1(){document.getElementById('logo').src='./image/roke1.png';}
-function imgchange2(){document.getElementById('logo').src='./image/roke2.png';}
+function imgchange1(){document.getElementById('roke').src='./image/roke1.png';}
+function imgchange2(){document.getElementById('roke').src='./image/roke2.png';}
--- ex6-2.js
+++ ex6-2.js
@@ -1,2 +1,2 @@
-function imgchange1(){document.getElementById('logo').src='./image/sakurei1.png';}
-function imgchange2(){document.getElementById('logo').src='./image/sakurei2.png';}
+function imgchange1(){document.getElementById('sakurei').src='./image/sakurei1.png';}
+function imgchange2(){document.getElementById('sakurei').src='./image/sakurei2.png';}
