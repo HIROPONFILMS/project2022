fb1d5f98f583c5086e784b7800815a00ede311bd
HIROPONFILMS <zm19291@josai.ac.jp>
2022-02-04 13:34:38 +0000

    12345

---
+++ ex6-1.js
@@ -0,0 +1,2 @@
+function imgchange1(){document.getElementById('logo').src='./image/roke1.png';}
+function imgchange2(){document.getElementById('logo').src='./image/roke2.png';}
---
+++ ex6-2.js
@@ -0,0 +1,2 @@
+function imgchange1(){document.getElementById('logo').src='./image/sakurei1.png';}
+function imgchange2(){document.getElementById('logo').src='./image/sakurei2.png';}
